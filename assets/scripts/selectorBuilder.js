352711a8934aa9c3a7cb855adab53eed13b47f82
Tomtoxi44 <tommy.angibaud@gmail.com>
2023-03-17 14:03:53 +0100

    Correction filtre charl et tommy

--- assets/scripts/selectorBuilder.js
+++ assets/scripts/selectorBuilder.js
@@ -1,46 +1,41 @@
-const cards = document.querySelector(".flip-card");
-//import restaurant from "/tableau";
+// const cards = document.querySelector(".flip-card");
+// import restaurant from "/tableau";
 
 function createCard(name, picture, description, adress, link) {
+  const card = document.createElement("div");
+  card.classList.add("flip-card-inner");
+  cards.appendChild(card);
 
+  const cardHeader = document.createElement("div");
+  cardHeader.classList.add("flip-card-front");
+  card.appendChild(cardHeader);
 
-    const card = document.createElement("div");
-    card.classList.add("flip-card-inner");
-    cards.appendChild(card);
-  
-    const cardHeader = document.createElement("div");
-    cardHeader.classList.add("flip-card-front");
-    card.appendChild(cardHeader);
-  
-    const cardImg = document.createElement("div");
-    cardImg.style.backgroundImage = `url(${picture})`;
-    cardImg.classList.add("card-picture");
-    cardHeader.appendChild(cardImg);
-  
-    const cardBody = document.createElement("div");
-    cardBody.classList.add("card-container");
-    card.appendChild(cardBody);
+  const cardImg = document.createElement("div");
+  cardImg.style.backgroundImage = `url(${picture})`;
+  cardImg.classList.add("card-picture");
+  cardHeader.appendChild(cardImg);
 
-    const cardTitle = document.createElement("p");
-    cardTitle.classList.add("card-title");
-    cardTitle.innerHTML = name;
-    cardBody.appendChild(cardTitle);
-  
-    const cardDescription = document.createElement("p");
-    cardDescription.classList.add("card-description");
-    cardDescription.innerHTML = description;
-    cardBody.appendChild(cardDescription);
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("card-container");
+  card.appendChild(cardBody);
 
-    const cardAddress = document.createElement("p");
-    cardDescription.classList.add("adress");
-    cardDescription.innerHTML = adress;
-    cardBody.appendChild(cardAddress);
+  const cardTitle = document.createElement("p");
+  cardTitle.classList.add("card-title");
+  cardTitle.innerHTML = name;
+  cardBody.appendChild(cardTitle);
 
-    const cardLink = document.createElement("p");
-    cardDescription.classList.add("button");
-    cardDescription.innerHTML = link;
-    cardBody.appendChild(cardLink);
-  }
+  const cardDescription = document.createElement("p");
+  cardDescription.classList.add("card-description");
+  cardDescription.innerHTML = description;
+  cardBody.appendChild(cardDescription);
 
+  const cardAddress = document.createElement("p");
+  cardDescription.classList.add("adress");
+  cardDescription.innerHTML = adress;
+  cardBody.appendChild(cardAddress);
 
-//export default carding;
+  const cardLink = document.createElement("p");
+  cardDescription.classList.add("button");
+  cardDescription.innerHTML = link;
+  cardBody.appendChild(cardLink);
+}
