63ee65a7bf05bc29b41b8767973562ff7abf4aaf
Tomtoxi44 <tommy.angibaud@gmail.com>
2023-03-17 11:10:21 +0100

    ajout du titre sur les logos

--- assets/scripts/tableau.js
+++ assets/scripts/tableau.js
@@ -4,9 +4,9 @@ const restaurant = [
     // Arancini n°0
     {
       name: "SUPPLI FACTORY",
-      picture: "",
+      picture: "../images/suppli_factory.jpg",
       description:
-        "BŒUF BRAISÉ Joue de bœuf braisée, compotée d’oignons,  tomates confites et comté AOP",
+        "Une cuisine de qualité, savoureuse, faite à partir de produits bruts. BŒUF BRAISÉ Joue de bœuf braisée, compotée d’oignons,  tomates confites et comté AOP",
       adress: "1 Rue de Jemmapes, 44000 Nantes",
       link: "http://supplifactory.com/la-carte/",
     },
@@ -15,22 +15,25 @@ const restaurant = [
     // Asiatique n°1
     {
       name: "ICHIGO ICHIE",
-      picture: "",
-      description: "",
+      picture: "../images/ichigo_ichie.jpg",
+      description:
+        "Le chef cuisine devant vous des Ramens , Gyoza, Karaage ou encore  Edamane !",
       adress: "31 rue Fouré 44000 Nantes",
       link: "https://ichigoichie.fr",
     },
     {
       name: "LE DIAMANT D ASIE",
-      picture: "",
-      description: "",
+      picture: "../images/le_diamant_d_asie.jpg",
+      description:
+        "Evadez vous en Tahilande et Vietnam avec ces spécialités : bo bun, porc au caramel,  crevettes sautées ou encore soupe Pho !",
       adress: "33 chaussée de la Madeleine 44000 Nantes",
       link: "https://le-diamant-dasie.business.site/?utm_source=gmb&utm_medium=referral",
     },
     {
       name: "IZAKAYA JOYI",
-      picture: "",
-      description: "",
+      picture: "../images/izakaya_joyi.jpg",
+      description:
+        " meilleur chef sushi de France 2015, GYUDON, RAMEN , UDON , SASHIMI, SOUPE , ATSUMONO",
       adress: "4 Rue de Colmar, 44000 Nantes",
       link: "https://www.izakayajoyi.fr/",
     },
@@ -39,71 +42,81 @@ const restaurant = [
     // Brasserie n°2
     {
       name: "LA RECRE",
-      picture: "",
-      description: "",
+      picture: "../images/la_recre.jpg",
+      description:
+        "Decouvrez nos spécialités : grillades au feu de bois, nuggets et lasagne de legumes, côte de boeuf , camembert roti, ou encore nos  Burgers !",
       adress: "5 rue Baron 44000 Nantes",
       link: "https://larecrenantes.fr/",
     },
     {
       name: "NAPOLEONE",
-      picture: "",
-      description: "",
+      picture: "../images/napoleone.jpg",
+      description:
+        "Un vrai Bistrot - Tapas : Venez déguster notre cochon iberique, hallouni grillé, cuisine Faite Maison parmi 3 plats 3 entrées 3 desserts chaque semaine !",
       adress: "1 rue Baron 44000 Nantes",
       link: "http://bistrot-napo.com/?#cart",
     },
     {
       name: "LE LION ET L AGNEAU",
-      picture: "",
-      description: "",
+      picture: "../images/le_lion_et_l_agneau.jpg",
+      description:
+        "Cuisine à l'accent du Sud Ouest : Rôti à la broche à partager, boeuf angus, Presa Iberique, Poisson du moment ou encore ravioles de  langoustine et saint  jacques !",
       adress: "40 rue Fouré 44000 Nantes",
       link: "https://www.le-lion-et-lagneau.fr/",
     },
     {
       name: "PLAN, B",
-      picture: "",
-      description: "",
+      picture: "../images/plan_b.jpg",
+      description:
+        "Venez découvrir nos plats faits maison, variétés de Burgers, la pêche du jour,  tataki de thon, poke bowl, fish and chips ou tiramisu !",
       adress: "29 Rue Fouré, 44000 Nantes",
       link: "https://www.planbnantes.fr/",
     },
     {
       name: "LA MANGOUSTE",
-      picture: "",
-      description: "",
+      picture: "../images/la_mangouste.jpg",
+      description:
+        "Restaurant , Bar à cocktails, Vins naturels : 3 plats 3 entrées 3 desserts à déguster sans oublier nos succulentes Pizzas à emporter !",
       adress: "30 Rue Fouré, 44000 Nantes",
       link: "https://lamangouste.net/",
     },
     {
       name: "LE MAGELLAN",
-      picture: "",
-      description: "",
+      picture: "../images/le_magellan.jpg",
+      description:
+        "Venez déguster de la cuisine traditonnelle fait maison comme l'excellente blanquette de veau ou saucisse au Muscadet, le menu change chaque semaine !",
       adress: "57 quai Magellan 44000 Nantes",
       link: "https://lemagellannantes.wixsite.com/lemagellan",
     },
     {
       name: "LE CAFE DU MARCHE",
-      picture: "",
-      description: "",
+      picture: "../images/le_cafe_du_marche.jpg",
+      description:
+        "La cuisine bistrot nantaise avec uniquement des produits locaux, venez goûter notre Kig ha farz, délicieux pot au feu breton !",
       adress: "2 Rue de Mayence, 44000 Nantes",
       link: "https://www.lecafedumarche.com/",
     },
     {
       name: "PAPILL",
-      picture: "",
-      description: "",
+      picture: "../images/papill.jpg",
+      description:
+        "PAPILL' c'est aussi des prestations annexes sur place ou à l'extérieur chez vous, dans votre entreprise, dans un lieu insolite ! Plat du jour 6,90e !",
       adress: "8 Rue de Saverne, 44000 Nantes",
       link: "https://www.papill.fr/carte",
     },
     {
       name: "GUINDAILLE",
-      picture: "",
-      description: "",
+      picture: "../images/guindaille.jpg",
+      description:
+        "Ton nouveau bar à manger débarque enfin sur Nantes, à Rue Fouré dans un chouette quartier : dégustez notre pitartiflette,  Poire Angys grillée et fumée ou encore  Morbier gratiné !",
       adress: "36 Rue Fouré, 44000 Nantes",
       link: "https://www.guindaille.fr/menu",
     },
     {
       name: "LES VOISINS",
-      picture: "",
-      description: "",
+      picture: "../images/les_voisins",
+      description:
+        "Le bar restaurant Les Voisins vous propose une carte variée qui plaira à tous les gouts ! BURGER VG sauté de veau marengo andouillette grillée TAPAS",
       adress: "12 rue de Mayence 44000 Nantes",
       link: "https://www.lesvoisins-nantes.fr/notre-carte",
     },
@@ -112,15 +125,17 @@ const restaurant = [
     // Burger n°3
     {
       name: "ERNEST INN",
-      picture: "",
-      description: "",
+      picture: "../images/ernest_inn.jpg",
+      description:
+        "Une faim de loup? venez déguster nos burgers composés uniquement de produits frais de saison, accompagnés de frites maison bien entendu !",
       adress: "11 allée de la maison rouge 44000 Nantes",
       link: "https://ernest-inn.com/",
     },
     {
       name: "LES BURGERS DE PAPA",
-      picture: "",
-      description: "",
+      picture: "../images/les_burgers_de_papa.jpg",
+      description:
+        "Les Burgers De Papa c’est d’abord les burgers d’un papa ! BUN BOULANGER, prdouits locaux frais , Frites Maison Burger le Jean claude Dusse la Biquette",
       adress: "5 Rue de Colmar, 44000 Nantes",
       link: "https://lesburgersdepapa.fr/",
     },
@@ -129,8 +144,9 @@ const restaurant = [
     // Creperie n°4
     {
       name: "CREPIZZ",
-      picture: "",
-      description: "",
+      picture: "../images/crepizz.jpg",
+      description:
+        "Vous aimez les galettes ? Vous aimez les pizzas? Crepizz a fait le mélange pour vous !",
       adress: "11 allée de la maison rouge 44000 Nantes",
       link: "https://crepizz-nantes.fr/",
     },
@@ -139,8 +155,9 @@ const restaurant = [
     // Fruit de mers n°5
     {
       name: "LE MORSE",
-      picture: "",
-      description: "",
+      picture: "../images/le_morse.jpg",
+      description:
+        "Envie d'air marin ? Venez découvrir la street sea food et le bar à fruits de mer à votre disposition !",
       adress: "1 allée de la maison Rouge 44000 Nantes",
       link: "https://le-morse.eatbu.com/?lang=fr",
     },
@@ -149,22 +166,25 @@ const restaurant = [
     // Gastronomique n°6
     {
       name: "L ATELIER D ALAIN",
-      picture: "",
-      description: "",
+      picture: "../images/l_atelier_d_alain.jpg",
+      description:
+        "Régalez vous de produits frais et de saison travaillés de manière gastronomique !",
       adress: "24 rue des olivettes 44000 Nantes",
       link: "https://www.facebook.com/restaurant.atelier.alain",
     },
     {
       name: "MAISON BARON LEFEVRE",
-      picture: "",
-      description: "",
+      picture: "../images/maison_baron_lefevre.jpg",
+      description:
+        "Decouvrez l'art culinaire d'un maître restaurateur renommé ! Des produits frais et de saison à la carte chaque semaine ",
       adress: "33 rue de rieux 44000 Nantes",
       link: "https://www.baron-lefevre.fr//",
     },
     {
       name: "OMIJA",
-      picture: "",
-      description: "",
+      picture: "../images/omija.jpg",
+      description:
+        "Omija est une baie coréenne, connue pour réunir cinq saveurs en harmonie :le salé, le sucré, l’acide, l’amer et le piquant (lievre, veau,maquereau, carrelet, fois gras, le bar ,canard)",
       adress: "54 Rue Fouré, 44000 Nantes",
       link: "https://www.omija.fr/menus",
     },
@@ -173,8 +193,9 @@ const restaurant = [
     // India n°7
     {
       name: "PALAIS DE L INDE",
-      picture: "",
-      description: "",
+      picture: "../images/palais_de_l_inde.jpg",
+      description:
+        "Découvrez la cuisine indienne : cheese naan, poulet Tikka, butter chicken , pakora, dall ou encore Palak Panir !",
       adress: "8 Rue de Mayence, 44000 Nantes",
       link: "https://palais-de-l-inde-nantes.eatbu.com/?lang=fr",
     },
@@ -183,8 +204,9 @@ const restaurant = [
     // Pates n°8
     {
       name: "PAPA PASTA",
-      picture: "",
-      description: "",
+      picture: "../images/papa_pasta.jpg",
+      description:
+        " Envie de voyager ? l'italie n'a jamais été aussi proche de chez vous  nos PATES carbonara, à la truffe, tiramisu maison raviolis maison",
       adress: "27 Rue Fouré, 44000 Nantes",
       link: "https://www.ubereats.com/fr/store/papa-pasta/MrcTZ3bkSi-6XRthLLywuQ?utm_campaign=place-action-link&utm_medium=organic&utm_source=google ",
     },
@@ -193,8 +215,9 @@ const restaurant = [
     // Pizza n°9
     {
       name: "PAZZI",
-      picture: "",
-      description: "",
+      picture: "../images/pazzi.jpg",
+      description:
+        "Venez vous réchauffer le cœur da i PAZZI !Partagez vos pizze et goûtez l’ivresse des vins siciliens ! ",
       adress: "21 Rue Fouré, 44000 Nantes",
       link: "https://pazzinantes.fr/",
     },
@@ -203,8 +226,9 @@ const restaurant = [
     // Sandwich n°10
     {
       name: "SUBWAY",
-      picture: "",
-      description: "",
+      picture: "../images/subway.jpg",
+      description:
+        "Sandwich préparés sous vos yeux, selon vos envies du moment ! ",
       adress: "2 rue de saverne 44000 Nantes",
       link: "https://www.subway.com/fr-fr",
     },
@@ -213,10 +237,10 @@ const restaurant = [
     // Vegan n°11
     {
       name: "LA FIERE CAROTTE ",
-      picture: "",
+      picture: "../images/la_fiere_carotte.jpg",
       description: "",
       adress: "4 rue Baron 44000 Nantes",
-      link: "",
+      link: "CAROTTE RAPEE ",
     },
   ],
 ];
