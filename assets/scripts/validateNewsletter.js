9390a8cfb3e4a54a334f31e0c87ee1a2ac926c72
seboubou <sebats@outlook.fr>
2023-03-22 14:26:12 +0100

    centrage de la pop up au click + placement de la pop pop au declick

--- assets/scripts/validateNewsletter.js
+++ assets/scripts/validateNewsletter.js
@@ -1,23 +1,23 @@
 function createPopup() {
-    const popup = document.querySelector(".popup-title");
-    let email = document.querySelector(".barre").value;
-    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    const a = document.createElement("a");
-    console.log(email);
-    
-    if(email.match(mailformat)){
-        a.href = "#popup";
-        a.click();    
-        popup.innerHTML = `Votre email : ${email} a été enregistré avec succès`;
-        
-    }
-    else {
-        const popupInvalideEmail = document.querySelector(".popup-description");
-        a.href = "#popup";
-        a.click();    
-        popup.innerHTML = `Votre email : ${email} est invalide :(`;
-        popupInvalideEmail.innerHTML = `Dommage`;
-   
-  }}
+  const popup = document.querySelector(".popup-title");
+  let email = document.querySelector(".barre").value;
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  const a = document.createElement("a");
 
-  document.querySelector("#button-submit").addEventListener("click", createPopup)
+  const popupInvalideEmail = document.querySelector(".popup-description");
+
+  if (email.match(mailformat)) {
+    a.href = "#popup";
+    a.click();
+    popup.innerHTML = `Votre email : ${email} a été enregistré avec succès`;
+    popupInvalideEmail.innerHTML = `Félicitation`;
+  } else {
+    const popupInvalideEmail = document.querySelector(".popup-description");
+    a.href = "#popup";
+    a.click();
+    popup.innerHTML = `Votre email : ${email} est invalide :(`;
+    popupInvalideEmail.innerHTML = `Dommage`;
+  }
+}
+
+document.querySelector("#button-submit").addEventListener("click", createPopup);
