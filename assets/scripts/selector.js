1b1a9803644a273562677fc3bb6eb0b2e80753a1
Tomtoxi44 <tommy.angibaud@gmail.com>
2023-03-24 12:08:11 +0100

    Possibilité de garder le background color sur un click et ajout d'une carte par défaut

--- assets/scripts/selector.js
+++ assets/scripts/selector.js
@@ -1,26 +1,45 @@
 const filtres = document.querySelectorAll(".filtreButton");
+console.log(filtres)
 import createCard from "./selectorBuilder.js";
 import restaurant from "./tableau.js";
 const cardsContainer = document.querySelector(".shopList");
 const italien = document.querySelector(".Italien");
 // variable pour afficher ou nom les cartes
 
-const createCards = () => {};
+const createCards = () => { 
 
-createCards();
+  filtres.forEach((filtre) => {   
+    filtre.addEventListener("click", (e) => {
 
-filtres.forEach((filtre) => {
-  filtre.addEventListener("click", (e) => {
-    let btn = e.target.value;
+      filtres.forEach((filtre) => {
+        filtre.style.backgroundColor = "#FCF4E8";
+      })
 
-    console.log(btn);
-    cardsContainer.innerHTML = "";
+      let btn = e.target.value;
+  
 
-    restaurant.forEach((resto) => {
-      if (btn === resto.type) {
-        cardsContainer.innerHTML += createCard(resto);
-      }
+      console.log(btn);
+      cardsContainer.innerHTML = "";
+
+      restaurant.forEach((resto) => {
+        if (btn === resto.type) {
+          cardsContainer.innerHTML += createCard(resto);
+          filtre.style.backgroundColor = "#DBAE3E"
+          
+        }
+      });
     });
   });
-});
+};
 
+
+const pardefaut = () => {
+  restaurant.forEach((resto) => {
+  if (resto.type === "Vegan"){
+    cardsContainer.innerHTML += createCard(resto)
+  }})
+}
+
+
+pardefaut();
+createCards();
